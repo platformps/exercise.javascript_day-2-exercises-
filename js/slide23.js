ce6e4a150426e748489ea5761bd4689e681de16e
unknown <dyirchott@hotmail.com>
2020-06-30 15:38:18 -0400

    cleaned up code and made slide 23 only go from lowest to highest number

--- js/slide23.js
+++ js/slide23.js
@@ -1,25 +1,19 @@
 var num1;
 var num2;
-//do {
     num1 = prompt("Enter the first of two numbers");
     num2 = prompt("Enter the second of the numbers");
 
     if (num1 > num2) {
         var L = num2;
         var U = num1;
-        for (U = num1; U > L; U--) {
-            console.log("number: " + U);
-            document.write("--> " + U);
+        for (L = num2; L < U; L++) {
+            console.log(L);
         }
     }
     if (num2 > num1) {
         var L = num1;
         var U = num2;
         for(L = num1; L < U; L++) {
-            console.log("number: " + L);
-            document.write("--> " + L);
+            console.log(L);
         }
     }
-    console.log("tadaaaa!");
-    document.write("tadaaaa!");
-//} while (2 == 2);
