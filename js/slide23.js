26c80183da8bfcdbe8f8176ccdab30dbb5617e6b
Jose Cruz <josec300@live.com>
2020-06-30 13:06:23 -0400

    finished slide 22 and, slide 23

---
+++ js/slide23.js
@@ -0,0 +1,7 @@
+let lower=prompt("Please enter L: ");
+let upper=prompt("Please enter U: ");
+
+for(let i=lower;i<upper;i++)
+{
+    console.log(i)
+}
---
+++ js/slide24.js
@@ -0,0 +1,19 @@
+let firstArray=new Array(10);
+let position=prompt("Enter a position in the index from 0-9")
+let choice=prompt("Enter the number you wan to change the value in this index")
+
+//fills all values in the array with 1's
+for(let i=0;i<10;i++)
+{
+    firstArray[i]=1;
+}
+
+//prints all items in the array"
+
+console.log(firstArray);
+
+//asks the user to input  the cell  index i and change it to a new value in the range  of array (0 to 10)
+// in a loop that keeps going until its not in range exit the javascript
+// if 
+
+while()
