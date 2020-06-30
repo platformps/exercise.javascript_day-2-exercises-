83c6eed8289bdbc572870ba1af9c5d74f542be8f
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-06-30 18:08:09 -0400

    Second Commit

---
+++ js/slide23.js
@@ -0,0 +1,28 @@
+
+
+    let rangestart = "";
+    let rangestop = "";
+    let output = "";
+    
+
+
+    {
+     rangestart =  prompt("Enter range start number");
+     {
+         rangestop = prompt("Enter range stop number");
+         for(let i = rangestart; i <= rangestop-1; i++) 
+         {
+             output+=i + " ";
+         
+         }
+         console.log(output);   
+             
+         
+     }
+     
+}
+    
+
+
+
+
--- js/slide26.js
+++ js/slide26.js
@@ -2,13 +2,13 @@ function fibonacci(someInteger){
     if(someInteger == 0){
         return 0;
     }
-    if(someInteger == 1){
+    if(someInteger == 1) {
         return 1;
     }
-    return fibonacci(someInteger-1)+fibonacci(someInteger-2);
+    return fibonacci(someInteger-1) + fibonacci(someInteger-2);
 
 }
 
-for(let i=0;i<59;i++){
+for(let i=0; i<59; i++){
     console.log(fibonacci(i));
 }
