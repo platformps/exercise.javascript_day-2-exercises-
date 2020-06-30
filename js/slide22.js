1b700a85a89a704038bce68b00e87867cce20111
Resilient08 <58645288+Resilient08@users.noreply.github.com>
2020-06-30 16:21:01 -0400

    updates to slide 22

---
+++ js/slide22.js
@@ -0,0 +1,4 @@
+var userInput;
+do {userInput=prompt ("Enter a number between 0 and 100");
+} while(userInput > 0 && userInput < 100);
+ alert ("The square of the number is" + userInput + userInput);
