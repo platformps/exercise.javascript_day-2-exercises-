5bedbb60e18c120447280b0430d7216661ad8430
Ezra Vance <3zravance@gmail.com>
2020-06-30 12:33:48 -0400

    slide 23 done.

---
+++ js/slide23.js
@@ -0,0 +1,8 @@
+var upperIn;
+var lowerIn;
+
+lowerIn = prompt("Enter a low range integer.");
+upperIn = prompt("Enter a high range integer. Bigger than the first.");
+for(var i = lowerIn; lowerIn < upperIn; lowerIn++){
+    console.log(lowerIn);
+}
