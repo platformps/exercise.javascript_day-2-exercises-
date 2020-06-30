938becde1ccfd716fc5858a602fb24a9f8790634
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-06-30 16:21:00 -0400

    First commit

---
+++ js/slide26.js
@@ -0,0 +1,14 @@
+function fibonacci(someInteger){
+    if(someInteger == 0){
+        return 0;
+    }
+    if(someInteger == 1){
+        return 1;
+    }
+    return fibonacci(someInteger-1)+fibonacci(someInteger-2);
+
+}
+
+for(let i=0;i<59;i++){
+    console.log(fibonacci(i));
+}
