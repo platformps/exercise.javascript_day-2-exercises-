111e47308dc318830ab2bacf54418df2b90866e2
Julia Waclawek <julie.waclawek@gmail.com>
2020-07-01 11:55:01 -0400

    first commit

---
+++ js/slide23.js
@@ -0,0 +1,10 @@
+//input two integers for the lower and upper limits of the interval
+let L = prompt("Please enter L: ");
+let U = prompt("Please enter U: ");
+let rangeLU = " ";
+
+//looping from L to U
+for (let i = 0; L < i < U; i++); {
+    console.log(rangeLU = i + " ");
+}        
+alert("The integers in the range L and U are: " + (rangeLU));
---
+++ js/slide24.js
@@ -0,0 +1,15 @@
+var myData = new Array(10); //create an array
+userInput = prompt("Enter cell index i = v");
+function printArray(myData); {
+for(var i = 1; i < 11; i ); { //using a for loop
+    console.log(myData [i]);
+}
+}
+var v = new myData;
+while( 0 < i < 10);{ 
+    do console.log(myData [v]);
+}
+ if(!0 < i < 10 == userInput); {
+    console.log(myData[v]);
+       break;
+}
