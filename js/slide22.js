0f8ba1ace0858bba22f3bd4c4a287818d2f1ac93
David Frary <fraryd@gmail.com>
2020-06-30 22:06:10 -0400

    finished edits on 'slide22.js'

--- js/slide22.js
+++ js/slide22.js
@@ -9,7 +9,7 @@
 
 let userInput = "";
 while (userInput = prompt ("Enter a number between 1 and 100:")){
-  if (isNaN(userInput) || userInput < 0 || userInput > 100){
+  if (isNaN(userInput) || userInput < 1 || userInput > 100){
     alert ("Re-enter number between 1 and 100:");
   } else {
     console.log(userInput);
