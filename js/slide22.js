b78251ca46337c432d4551cc48000f7f50c5e9a6
David Frary <fraryd@gmail.com>
2020-07-02 15:22:40 -0400

    finished edits on 'slide23.js'

--- js/slide22.js
+++ js/slide22.js
@@ -1,12 +1,4 @@
 
-
-//Do While Statement
-// do{
-//   statements
-// }
-// while (condition);
-
-
 let userInput = "";
 while (userInput = prompt ("Enter a number between 1 and 100:")){
   if (isNaN(userInput) || userInput < 1 || userInput > 100){
---
+++ js/slide23.js
@@ -0,0 +1,11 @@
+
+
+
+let userInput1=prompt("Please enter L: ");
+let userInput2=prompt("Please enter U: ");
+
+for(let i=userInput1;i<=userInput2;i++)
+
+{
+    console.log(i)
+} 
